f3cbaea5e32a9b4a1b8184132eb233ea1d9808a7
sobambo olusola <108358797+SobamboS@users.noreply.github.com>
2022-11-03 17:35:45 +0100

    Update InitArray2.java

--- src/Chapter7/Example/InitArray2.java
+++ src/Chapter7/Example/InitArray2.java
@@ -1,2 +1,13 @@
-package Chapter7.Example;public class InitArray2 {
+package Chapter7.Example;
+
+public class InitArray2 {
+    public static void main ( String[] args ) {
+        int[] array = {10, 23, 34, 45, 56, 67, 78, 89, 90};
+
+        System.out.printf ( "%s%8s%n", "Index", "Value" ); // column headings
+        // output each array element's value
+        for (int counter = 0; counter < array.length; counter++) {
+            System.out.printf ( "%5d%8d%n", counter, array[counter] );
+        }
+    }  
 }
