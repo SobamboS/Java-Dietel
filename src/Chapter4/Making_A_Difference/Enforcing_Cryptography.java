bdf907beedf55610408652df0ce8b8929dd8d789
SobamboS <sobamboolusola1152@gmail.com>
2022-12-08 18:09:24 +0100

    Enforcing Cryptography

--- src/Chapter4/Making_A_Difference/Enforcing_Cryptography.java
+++ src/Chapter4/Making_A_Difference/Enforcing_Cryptography.java
@@ -1,31 +1,30 @@
 package Chapter4.Making_A_Difference;
 
+
+
 import java.util.Scanner;
 
 public class Enforcing_Cryptography{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
 
-int firstNumber =0;
-int secondNumber=0;
-int thirdNumber=0;
-int fourthNumber=0;
+        System.out.println("Enter your 4 digit pin :");
+        int firstNumber= input.nextInt();
+        int secondNumber=input.nextInt();
+        int thirdNumber=input.nextInt();
+        int fourthNumber=input.nextInt();
 
 
+       int pin1 = (firstNumber + 7) % 10;
+        int pin2 = (secondNumber +7) % 10;
+        int pin3 =(thirdNumber+7) % 10;
+       int pin4= (fourthNumber+7 )%10;
 
-        while (integer<5) {
-            System.out.println("Enter 4 numbers :");
-             firstNumber=input.nextInt();
-             secondNumber=input.nextInt();
-             thirdNumber=input.nextInt();
-             fourthNumber=input.nextInt();
+       pin1 = pin3;
+       pin2 = pin4;
+       pin3= pin1;
+       pin4 = pin2;
 
-        }
-            firstNumber+=7;
-            secondNumber +=7;
-            thirdNumber+=7;
-            fourthNumber+=7;
-
-        System.out.printf("%d  %d  %d %d",firstNumber, secondNumber, thirdNumber, fourthNumber);
+        System.out.printf("Encrypted pin is : %d%d%d%d", pin1, pin2, pin3, pin4);
     }
 }
---
+++ src/Chapter4/Making_A_Difference/testing.java
@@ -0,0 +1,27 @@
+package Chapter4.Making_A_Difference;
+
+public class testing{
+    public static void main(String[] args){
+
+//        String number="12343455";
+//        int why = 0;
+//
+//        int hello=0;
+//        for(int i=0;i<number.length();i++){
+//            hello=Integer.parseInt(number);
+//            System.out.println(number.charAt(i));
+//        }
+//        System.out.println(hello);
+//        System.out.println(why);
+
+
+
+    }
+
+
+}
+
+
+
+
+
