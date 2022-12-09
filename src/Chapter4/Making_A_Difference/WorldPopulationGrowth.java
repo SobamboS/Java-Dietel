2816a0eb2c180a107af3ae44c2e6c12e16370773
SobamboS <sobamboolusola1152@gmail.com>
2022-12-09 02:19:34 +0100

    This program calculates the world expected growth in 75 years with growth rate of 0.85, then determines when it would have been doubled

--- src/Chapter4/Making_A_Difference/WorldPopulationGrowth.java
+++ src/Chapter4/Making_A_Difference/WorldPopulationGrowth.java
@@ -5,18 +5,24 @@ public class WorldPopulationGrowth{
 
         long world_population=7_000_000_000L;
         double world_growth_rate=0.85;
-        int count=1;
+        long growth =0;
 
-        long growth=0;
-        while (count<5) {
-            System.out.println(count);
-            growth=(long) (world_growth_rate*world_population);
-            count++;
+        long increase=0;
+        int year;
+
+
+        System.out.print("Year      Growth     Rate");
+        for (year = 0; year  < 76 ; year++){
+             growth=(long) (world_growth_rate * world_population);
+            increase +=growth;
+        System.out.printf("%d%n       %d     %.1f%n",year, increase, world_growth_rate);
         }
 
-        System.out.println(growth);
+        if(increase == world_population *2  ){
 
+            System.out.println(growth);
+            System.out.println(year);
+        }
 
-        //System.out.print(world_population + "" + world_growth_rate);
     }
 }
