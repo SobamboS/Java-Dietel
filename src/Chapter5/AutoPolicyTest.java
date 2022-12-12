67f3398181576fdd6d015c6126020b1c2793c261
SobamboS <sobamboolusola1152@gmail.com>
2022-12-12 23:45:20 +0100

    ""

---
+++ src/Chapter5/AutoPolicyTest.java
@@ -0,0 +1,13 @@
+package Chapter5;
+
+import Chapter5.Example.AutoPolicy;
+
+public class AutoPolicyTest{
+    public static void main(String[] args){
+        AutoPolicy policy1 = new AutoPolicy(11111111, "Toyota Camry", "NJ");
+
+        AutoPolicy policy2 = new AutoPolicy(222222222, "Benz GLK", "PA");
+
+
+    }
+}
