8c12014595dbbd29a30b33044c8e666015ce3a99
SobamboS <sobamboolusola1152@gmail.com>
2022-12-16 00:20:56 +0100

    (Integers Divisible by 3) Write an application that calculates the sum of those integers between 1 and 30 that are divisible by 3.

--- src/Chapter5/DivisibleByThree.java
+++ src/Chapter5/DivisibleByThree.java
@@ -2,6 +2,14 @@ package Chapter5;
 
 public class DivisibleByThree{
     public static void main(String[] args){
+int add =0;
+        for(int i=3;i<=30;i++){
+            if(i%3==0){
+                add+=i;
+            }
+
+        }
+                System.out.println(add);
 
     }
 }
