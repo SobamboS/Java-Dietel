d0c9deb29f44d062f8faaf6c876d525f711e67a4
SobamboS <sobamboolusola1152@gmail.com>
2022-12-16 02:00:11 +0100

    Mystery

---
+++ src/Chapter5/Counting.java
@@ -0,0 +1,16 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+public class Counting{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+
+        for(int i=1;i<3;i++){
+            for(int j=1;j<5;j++){
+                System.out.print('*');
+                System.out.println("\n#####");
+            }
+        }
+    }
+}
