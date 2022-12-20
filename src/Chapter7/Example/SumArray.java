6b45806ce8d3a018f52b36cda4d966d4c8401d86
SobamboS <sobamboolusola1152@gmail.com>
2022-12-20 06:13:21 +0100

    ""

--- src/Chapter7/Example/SumArray.java
+++ src/Chapter7/Example/SumArray.java
@@ -4,8 +4,8 @@ public class SumArray {
     public static void main ( String[] args ) {
         int [] array = {12, 23, 34, 45, 56, 67, 78, 89, 90};
         int total = 0;
-        for (int counter = 0; counter < array.length; counter++){
-    total += array[counter];
+        for(int i : array){
+            total+=i;
         }
             System.out.printf ( "Total of array elements : %d%n", total);
 
