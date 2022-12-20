25ecea02fdccecbb08be362c27b0ab819ad67680
SobamboS <sobamboolusola1152@gmail.com>
2022-12-20 07:09:34 +0100

    ""

--- src/Chapter7/Example/InitArray2.java
+++ src/Chapter7/Example/InitArray2.java
@@ -1,2 +1,17 @@
-package Chapter7.Example;public class InitArray2 {
+package Chapter7.Example;
+
+
+import java.util.Arrays;
+
+public class InitArray2 {
+    public static void main(String[] args){
+        int[] array = {1,5,8,9,6,3,5,6,7,3,2,5,6,9,8,3,5,6,7};
+        Arrays.sort(array);
+
+
+
+        for(int i : array){
+            System.out.println(i);
+        }
+    }
 }
